bc363de43afb215ed6bcb698954500107a0968fe
honeybadger2788 <noe.carosella@gmail.com>
2023-03-09 16:16:00 -0300

    add loadBalancer and Feing

--- api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -1,14 +1,17 @@
 package com.dh.catalog.client;
 
+import com.dh.catalog.config.LoadBalancerConfiguration;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="api-movie",url = "http://localhost:8085/api-movie")
+@FeignClient(name="api-movie")
+@LoadBalancerClient(value="api-movie", configuration= LoadBalancerConfiguration.class)
 public interface MovieServiceClient {
 
 	@GetMapping("/api/v1/movies/{genre}")
