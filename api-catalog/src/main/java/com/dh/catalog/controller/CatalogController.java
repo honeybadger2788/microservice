f94261836ce21ceade087958cb5e60475b90ddf5
honeybadger2788 <noe.carosella@gmail.com>
2023-04-05 23:12:15 -0300

    add api-serie service client in catalog

--- api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
+++ api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
@@ -2,13 +2,13 @@ package com.dh.catalog.controller;
 
 import com.dh.catalog.client.MovieServiceClient;
 
+import com.dh.catalog.client.SerieServiceClient;
+import com.dh.catalog.model.Catalog;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/catalog")
@@ -16,13 +16,21 @@ public class CatalogController {
 
 	private final MovieServiceClient movieServiceClient;
 
-	public CatalogController(MovieServiceClient movieServiceClient) {
+	private final SerieServiceClient serieServiceClient;
+
+	public CatalogController(MovieServiceClient movieServiceClient, SerieServiceClient serieServiceClient) {
 		this.movieServiceClient = movieServiceClient;
+		this.serieServiceClient = serieServiceClient;
 	}
 
 	@GetMapping("/{genre}")
-	ResponseEntity<List<MovieServiceClient.MovieDto>> getGenre(@PathVariable String genre) {
-		return ResponseEntity.ok(movieServiceClient.getMovieByGenre(genre));
+	ResponseEntity<Catalog> getGenre(@PathVariable String genre) {
+		Catalog catalog = new Catalog(
+				genre,
+				movieServiceClient.getMovieByGenre(genre),
+				serieServiceClient.getSerieByGenre(genre)
+		);
+		return ResponseEntity.ok(catalog);
 	}
 
 }
