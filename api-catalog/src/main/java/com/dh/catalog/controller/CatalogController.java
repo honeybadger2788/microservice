abb788410e2be9311058875f3968ea8c084a24be
honeybadger2788 <noe.carosella@gmail.com>
2023-04-07 12:48:31 -0300

    add api-catalog data base

--- api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
+++ api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
@@ -1,36 +1,20 @@
 package com.dh.catalog.controller;
 
-import com.dh.catalog.client.MovieServiceClient;
-
-import com.dh.catalog.client.SerieServiceClient;
-import com.dh.catalog.model.Catalog;
+import com.dh.catalog.service.impl.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/catalog")
 public class CatalogController {
 
-	private final MovieServiceClient movieServiceClient;
-
-	private final SerieServiceClient serieServiceClient;
-
-	public CatalogController(MovieServiceClient movieServiceClient, SerieServiceClient serieServiceClient) {
-		this.movieServiceClient = movieServiceClient;
-		this.serieServiceClient = serieServiceClient;
-	}
+	@Autowired
+	CatalogService catalogService;
 
 	@GetMapping("/{genre}")
-	ResponseEntity<Catalog> getGenre(@PathVariable String genre) {
-		Catalog catalog = new Catalog(
-				genre,
-				movieServiceClient.getMovieByGenre(genre),
-				serieServiceClient.getSerieByGenre(genre)
-		);
-		return ResponseEntity.ok(catalog);
+	ResponseEntity<?> getGenre(@PathVariable String genre) {
+		return ResponseEntity.ok(catalogService.getCatalog(genre));
 	}
 
 }
---
+++ api-catalog/src/main/java/com/dh/catalog/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.dh.catalog.repository;
+
+import com.dh.catalog.model.Movie;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, Long> {
+    List<Movie> findAllByGenre(String genre);
+}
